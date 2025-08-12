dc2fba6f36b67c791d0ad934c6cf026562b728d0
Ignacio Molina <molinagranda@gmail.com>
2025-08-12 20:21:08 -0300

    Primer Proyecto JS - JS en un archivo aparte

---
+++ JavaScript/primerProyecto/main.js
@@ -0,0 +1,2 @@
+// Un console log es lo que se muestra en la consola, sirve a veces para poder revisar diversos datos que suelte nuestro codigo o para mostrar algo en la consola
+console.log("Soy un console log enviado desde el archivo main.js");
