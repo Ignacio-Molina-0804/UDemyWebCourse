871e71198c6100dd72e4fa12906c1236f2574fbf
Ignacio Molina <molinagranda@gmail.com>
2025-08-12 20:22:54 -0300

    Comentarios en JavaScript

--- JavaScript/primerProyecto/main.js
+++ JavaScript/primerProyecto/main.js
@@ -1,2 +1,17 @@
 // Un console log es lo que se muestra en la consola, sirve a veces para poder revisar diversos datos que suelte nuestro codigo o para mostrar algo en la consola
 console.log("Soy un console log enviado desde el archivo main.js");
+
+/// COMENTARIOS
+
+// Asi se hace un comentario de una sola linea.
+
+/*
+Asi
+Se
+Hace
+Un
+Comentario
+De
+Varias
+Lineas
+*/
