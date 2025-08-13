74fc1da1f8878e3e20f8cde5a15edf172de700b0
Ignacio Molina <molinagranda@gmail.com>
2025-08-13 15:03:18 -0300

    Tipos de Datos Ejemplificados

---
+++ JavaScript/tiposDeDatos/script.js
@@ -0,0 +1,11 @@
+// Numero Entero
+
+let frase = "Hola soy Ignacio"  // Cadena de texto - String
+let anio = 2025                 // Numero entero - Int
+let interes = 2.7               // Numero decimal - Float/Double
+let mayor = true                // Verdadero o Falso - Boleano
+let vacio                       // Undefined
+let nula = null                 // Valor nulo - Null
+
+let arrayFrutas = ["manzana", "Pera", "Tomate"]         // Array o Arreglo
+let heroe = {nombre: "Spiderman", universo: "Marvel"}   // Objeto
