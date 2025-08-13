9968e497d8317654ae2a9e8cdb47c406103ec4f0
Ignacio Molina <molinagranda@gmail.com>
2025-08-13 15:24:39 -0300

    Ejemplo Operadores Aritmeticos

--- JavaScript/operadores/script.js
+++ JavaScript/operadores/script.js
@@ -4,3 +4,32 @@ let interes = 2.7
 
 // Operador TypeOf (Operador que muestra de que tipo de dato es tu variable)
 console.log(typeof interes); // En este caso esto devuelve number 
+
+// Operadores aritmeticos
+
+let num1 = 15
+let num2 = 12.5
+
+let suma = numero1 + numero2
+let resta = numero1 - numero2
+let multi = numero1 * numero2
+let division = numero1 / numero2
+let resto = numero1 % numero2
+let potencia = numero1 ** numero2
+
+console.log("Suma = " + suma);
+console.log("Resta = " + resta);
+console.log("Multiplicacion = " + multi);
+console.log("Division = " + division);
+console.log("Resto = " + resto);
+console.log("Potencia = " + potencia);
+
+// Asignacion
+
+// Comparacion
+
+// Logicos
+
+// Cadena
+
+// Incremento y decremento
