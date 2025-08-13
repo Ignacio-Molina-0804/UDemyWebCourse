a0ce4ceac05f215a8d9bf424b9d2ce06e623ca17
Ignacio Molina <molinagranda@gmail.com>
2025-08-12 21:16:57 -0300

    Uso Basico, Concatenacion y Sustitucion

--- JavaScript/variables/script.js
+++ JavaScript/variables/script.js
@@ -10,3 +10,19 @@ let nombre2 = "Nacho"
 
 // Variable cuyo valor no cambia debido a que es un valor constante
 const nombre3 = "Nacho"
+
+/* --------------------------------------------------------------- */
+
+/// Uso de una variable
+
+// Uso basico
+console.log(nombre2);
+
+// Concatenacion
+let nombrelet_y_nombreconst = nombre2 + " " + nombre3
+console.log("Concatenacion = " + nombrelet_y_nombreconst);
+
+// Sustitucion
+nombre2 = "Pepe"
+console.log("Sustituacion = " + nombre2)
+
