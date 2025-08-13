6fd77f51325da4c37d9abaa6d914b079eef03036
Ignacio Molina <molinagranda@gmail.com>
2025-08-13 15:19:43 -0300

    TypeOf Explicado

---
+++ JavaScript/operadores/script.js
@@ -0,0 +1,6 @@
+let interes = 2.7
+
+/// Operadores
+
+// Operador TypeOf (Operador que muestra de que tipo de dato es tu variable)
+console.log(typeof interes); // En este caso esto devuelve number 
