41c6ebaffcc48f7539ec1980e110e097c85acdbf
Ignacio Molina <molinagranda@gmail.com>
2025-08-12 21:12:11 -0300

    Tipos de Variable

---
+++ JavaScript/variables/script.js
@@ -0,0 +1,12 @@
+alert("Variables en JS")
+
+/// Variables
+
+// Alcance general
+var nombre = "Nacho"
+
+// Alcance de bloque
+let nombre2 = "Nacho"
+
+// Variable cuyo valor no cambia debido a que es un valor constante
+const nombre3 = "Nacho"
