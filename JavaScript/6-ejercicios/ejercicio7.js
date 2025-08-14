b320a937d30210b3584edd687c414f67171bb69e
Ignacio Molina <molinagranda@gmail.com>
2025-08-14 19:09:17 -0300

    Ejercicio 7

---
+++ JavaScript/6-ejercicios/ejercicio7.js
@@ -0,0 +1,17 @@
+/* 
+
+Ejercicio 7
+
+Tenemos un circulo con un diametro de 18 cm
+
+Calcula al radio del circulo y dime cual es
+
+*/
+
+const diametro = 18
+
+let radio = diametro / 2
+
+console.log("Ejercicio 7");
+console.log(`El radio del circulo es de ${radio} cm`);
+
