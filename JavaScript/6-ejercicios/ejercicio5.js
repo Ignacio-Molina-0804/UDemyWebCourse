7e5fd2cd6f13f098c14f4eed214b442c5649db6b
Ignacio Molina <molinagranda@gmail.com>
2025-08-14 18:18:10 -0300

    Ejercicio 5

---
+++ JavaScript/6-ejercicios/ejercicio5.js
@@ -0,0 +1,19 @@
+/* 
+
+Ejercicio 5
+
+Tengo una tarta de queso con 16 porciones.
+
+Cada porcion vale 3.8 euros.
+
+¿Si vendo todas las porciones cuanto dinero gano en total?
+
+*/
+
+let porcionesTartaDeQueso = 16
+const precioPorPorcion = 3.8
+
+let totalDeDinero = porcionesTartaDeQueso * precioPorPorcion
+
+console.log("Ejercicio 5")
+console.log(`Si vendiera las ${porcionesTartaDeQueso} porciones de tarta de queso ganaria un total de ${totalDeDinero} euros.`);
