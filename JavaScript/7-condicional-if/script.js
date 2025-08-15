3b23f2ee870c964a92e07cf5c5d3e694bcbbf179
Ignacio Molina <molinagranda@gmail.com>
2025-08-15 20:32:35 -0300

    Condicional IF

---
+++ JavaScript/7-condicional-if/script.js
@@ -0,0 +1,16 @@
+/// Condicional IF
+// Un condicional IF es algo que como dice el nombre es una condicion, este se podria tomar como decir si tomo una coca, tenemos el true que seria en el caso de que la tomemos o el false en el caso de que no
+
+tomeUnaCoca = true
+
+if(tomeUnaCoca){
+
+    // Si se cumple la condicion
+    console.log("Hoy me tome una coca");
+
+} else {
+
+    // Si NO se cumple la condicion
+    console.log("Hoy no me tome una coca");
+
+}
