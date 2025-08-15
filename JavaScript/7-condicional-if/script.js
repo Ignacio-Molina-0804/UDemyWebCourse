9680c87705f0bdefc553ab888951a0d501a2ecf5
Ignacio Molina <molinagranda@gmail.com>
2025-08-15 20:41:04 -0300

    Otro Ejemplo de ELSEIF

--- JavaScript/7-condicional-if/script.js
+++ JavaScript/7-condicional-if/script.js
@@ -1,16 +1,33 @@
 /// Condicional IF
-// Un condicional IF es algo que como dice el nombre es una condicion, este se podria tomar como decir si tomo una coca, tenemos el true que seria en el caso de que la tomemos o el false en el caso de que no
+// Un condicional IF es algo que como dice el nombre es una condicion, este
 
-tomeUnaCoca = true
+//Ejemplo 1
 
-if(tomeUnaCoca){
+// Tengo que tener un condicional el cual me de un mensaje si tomamos una coca, tenemos el true que seria en el caso de que la tomemos o el false en el caso de que no.
 
-    // Si se cumple la condicion
-    console.log("Hoy me tome una coca");
+tomeUnaCoca = true;
 
+if (tomeUnaCoca) {
+  // Si se cumple la condicion
+  console.log("Hoy me tome una coca");
 } else {
+  // Si NO se cumple la condicion
+  console.log("Hoy no me tome una coca");
+}
 
-    // Si NO se cumple la condicion
-    console.log("Hoy no me tome una coca");
+// Ejemplo 2
 
+// Hacer el caso de ver si alguien en mayor o menor de edad
+
+let nombre = "Pepe";
+let edadPepe = 18;
+
+if (edadPepe >= 18) {
+  console.log(
+    `${nombre} es mayor de edad debido a que tiene ${edadPepe} años.`
+  );
+} else {
+  console.log(
+    `${nombre} no es mayor de edad debido a que tiene ${edadPepe} años.`
+  );
 }
