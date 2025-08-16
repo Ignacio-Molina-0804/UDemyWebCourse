00e2888eded02d3836dbb2752186acca56bbddd6
Ignacio Molina <molinagranda@gmail.com>
2025-08-15 21:05:12 -0300

    Switch Case

--- JavaScript/8-mas-condicionales/script.js
+++ JavaScript/8-mas-condicionales/script.js
@@ -1,6 +1,27 @@
 // Condicional Switch
 
+// Un switch en JavaScript (y en muchos otros lenguajes) es una estructura de control que sirve para ejecutar diferentes bloques de código según el valor de una variable o expresión.
 
+// En lugar de usar muchos if...else if...else, el switch compara el valor con diferentes casos (case) y ejecuta el bloque que coincida.
+
+let miDesayuno = "tortitas";
+
+switch (miDesayuno) {
+    case "tortitas":
+        console.log("Has elegido TORTITAS con SIROPE DE ARCE");
+        break;
+
+    case "huevos":
+        console.log("Has elegido huevos fritos con bacon");
+        break;
+
+    case "colacao":
+        console.log("Has elegido un Colacao con copos de avena");
+        break;
+
+    default:
+        console.log("Has elegido otro desayuno diferente");
+}
 
 // Condicional Ternario
 
