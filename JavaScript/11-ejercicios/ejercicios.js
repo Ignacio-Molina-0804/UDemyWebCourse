4ba44f67da067aade775aab178eeff8f1dd1fc37
Ignacio Molina <molinagranda@gmail.com>
2025-08-16 14:47:46 -0300

    Ejercicio 11

--- JavaScript/11-ejercicios/ejercicios.js
+++ JavaScript/11-ejercicios/ejercicios.js
@@ -1,1 +1,17 @@
+/*
+Ejercicio 11:
 
+Nuestros usuarios tienen perros.
+
+Y quieren saber que edad de perro tienen sus mascotas.
+
+Un año de humano equivale a siete de perro.
+
+Pregunta a los usuarios que edad tiene su perro y dile la edad canina.
+*/
+
+let edadHumana = prompt("Cual es la de edad de su perro? (En años humanos)")
+
+let conversionEdadAEdadPerro = edadHumana * 7
+
+alert(`La edad canina de su perro es de ${conversionEdadAEdadPerro} años perrunos.`);
