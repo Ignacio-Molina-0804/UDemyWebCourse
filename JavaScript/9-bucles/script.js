430f4d9d3e172e4a7ff7299a5a914d9ca9a0b3c7
Ignacio Molina <molinagranda@gmail.com>
2025-08-15 21:16:56 -0300

    Bucle For

---
+++ JavaScript/9-bucles/script.js
@@ -0,0 +1,18 @@
+// Bucle For
+// Un bucle es una estructura de control que ejecuta un bloque de código
+// varias veces de forma automática mientras se cumpla una condición
+
+/*
+for (inicialización; condición; incremento) {
+
+    // Código que se ejecuta repetidamente
+
+}
+*/
+
+let limits = 177;
+
+for(let contador = 8; contador != limits; contador++) {
+    // bloque de código que se ejecutará
+    console.log("Vamos por el numero: " + contador);
+}
