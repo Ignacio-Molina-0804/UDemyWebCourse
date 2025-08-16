e529355d52ea739f85487b1941c8e70cac2d268c
Ignacio Molina <molinagranda@gmail.com>
2025-08-16 14:33:55 -0300

    Prompts

--- JavaScript/10-ventanas/script.js
+++ JavaScript/10-ventanas/script.js
@@ -11,3 +11,7 @@ let pregunta = confirm("¿Estas seguro de que quieres continuar?");
 console.log(pregunta);
 
 // Prompts (ingreso de datos)
+
+let web = prompt("¿Qué web tienes?");
+
+console.log(web);
