d287654cc455457115ccdea6815df17c7cde581a
Ignacio Molina <molinagranda@gmail.com>
2025-08-18 00:02:42 -0300

    Ejercicio 19

--- JavaScript/11-ejercicios/ejercicios.js
+++ JavaScript/11-ejercicios/ejercicios.js
@@ -245,3 +245,19 @@ do {
     }
 
 } while (numeroParaAdivinar !== numeroRespuesta);
+
+/*
+
+Ejercicio 19:
+
+Muestra todos los numeros divisores de un numero que se mete en un prompt
+
+*/
+
+let numero = parseInt(prompt("Mete tu número y te digo los divisores", 1));
+
+for (let contador = 1; contador <= numero; contador++) {
+    if (numero % contador == 0) {
+        console.log(`El ${contador} es divisor de ${numero}`);
+    }
+}
