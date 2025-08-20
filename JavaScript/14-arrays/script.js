6dc8e28ef61182d48676d981015560c75cb850c7
Ignacio Molina <molinagranda@gmail.com>
2025-08-20 20:34:04 -0300

    Creacion ARRAY, Acceder a un elemento y tamaño del array

---
+++ JavaScript/14-arrays/script.js
@@ -0,0 +1,11 @@
+/// ARRAY, MATRICES Y ARREGLOS
+
+// Forma de crear un array
+let nombres = ["Ignacio", "Pepe", "Juan", "Pedro", "Maria"];
+//                 0         1       2       3        4
+
+// Acceder a un elemento del array
+console.log(nombres[0]);
+
+// Ver el tamaño del array
+console.log(nombres.length); // 5
