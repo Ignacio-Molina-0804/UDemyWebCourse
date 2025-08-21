63ca9907689d14228f021e9ddf89cc4cec94c301
Ignacio Molina <molinagranda@gmail.com>
2025-08-21 15:27:25 -0300

    Bucles Map, ForEach, ForIn, ForOf, For

--- JavaScript/14-arrays/script.js
+++ JavaScript/14-arrays/script.js
@@ -29,5 +29,52 @@ for (let i = 0; i < nombres.length; i++) {
 
     console.log("Usuario " + i + ": " + nombres[i]);
     
+}
+
+// Bucles para recorrer arrays
+
+console.log("*****************************")
+
+let actores = ["Robert Downey Jr.", "Chris Evans", "Scarlett Johansson", "Chris Hemsworth", "Tom Holland"];
+
+console.log("Listado de actores: ")
+
+// Bucle for
+
+for (let i = 0; i < actores.length; i++) {
+    console.log("Actor " + (i + 1) + ": " + actores[i]);
+}
+
+// Bucle For OF
+
+for (let actor of actores) {
+
+    console.log("Actor: " + actor);
 
 }
+
+// Bucle For IN
+
+for (let index in actores) {
+
+    console.log("Actor " + (parseInt(index) + 1) + ": " + actores[index]);
+    
+}
+
+// Bucle ForEach
+
+actores.forEach((actor, index) => {
+    console.log("Actor " + (index + 1) + ": " + actor);
+});
+
+// Bucle Map
+
+let actoresNuevos = actores.map(actor => {
+
+    console.log("Actor:" + actor); 
+
+    return actor + " (map)";
+})
+
+console.log("Actores Nuevos: ", actoresNuevos);
+
