358e56fe82792dd0376a1c0060697822db578cdb
Ignacio Molina <molinagranda@gmail.com>
2025-08-22 16:30:35 -0300

    Ejercicio 26

--- JavaScript/16-ejercicios/ejercicios.js
+++ JavaScript/16-ejercicios/ejercicios.js
@@ -40,3 +40,25 @@ let ingredientes = ["masa de pizza", "salsa de tomate", "queso mozzarella", "cho
 const receta = `Para preparar una pizza única, necesitas: ${ingredientes.join(", ")}. ¡Disfruta de esta deliciosa combinación!`;
 
 alert(receta);
+
+/*
+Ejercicio 26:
+
+Dado un array de palabras, cuenta cuántas letras tiene cada palabra
+y crea un nuevo array que contenga solo esos números.
+*/
+
+alert("Ejercicio 26: ")
+
+let palabras = ["manzana", "banana", "cereza", "durazno", "frambuesa"];
+let luchadores = ["Rey Mysterio", "John Cena", "The Undertaker", "Stone Cold", "Triple H"];
+
+function contarletras(datos) {
+
+    let longitudes = datos.map(palabra => palabra.length);
+
+    return longitudes;
+
+}
+
+console.log(contarletras(luchadores)); // [7, 6, 6, 7, 9]
