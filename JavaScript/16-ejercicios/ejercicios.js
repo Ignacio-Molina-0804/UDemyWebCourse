1013cee27c5731db64d7656619a0ccda1441f21a
Ignacio Molina <molinagranda@gmail.com>
2025-08-22 16:24:06 -0300

    Ejercicio 25

--- JavaScript/16-ejercicios/ejercicios.js
+++ JavaScript/16-ejercicios/ejercicios.js
@@ -8,6 +8,8 @@ Si acierta, muestra un mensaje de celebración;
 si no, muestra el número correcto.
 */
 
+alert("Ejercicio 24: ");
+
 let numerosAleatorios = Array.from({ length: 10 }, () => Math.floor(Math.random() * 100) + 1);
 
 let numeroUsuario = parseInt(prompt("Adivina un número entre 1 y 100:"));
@@ -21,3 +23,20 @@ if (numerosAleatorios.includes(numeroUsuario)) {
     alert("Lo siento, los numeros correctos eran: " + numerosAleatorios.join(", "));
 
 }
+
+/*
+Ejercicio 25:
+
+Crea un array de ingredientes para una receta absurda
+(como una pizza con chocolate y piña).
+
+Muestra la receta completa en una frase divertida.
+*/
+
+alert("Ejercicio 25: ")
+
+let ingredientes = ["masa de pizza", "salsa de tomate", "queso mozzarella", "chocolate", "piña", "pepperoni", "aceitunas", "pimientos", "cebolla", "albahaca"];
+
+const receta = `Para preparar una pizza única, necesitas: ${ingredientes.join(", ")}. ¡Disfruta de esta deliciosa combinación!`;
+
+alert(receta);
