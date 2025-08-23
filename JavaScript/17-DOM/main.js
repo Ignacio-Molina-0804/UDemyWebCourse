3cfeefbac1524bdc54e88295b87fb47ea48bb0a2
Ignacio Molina <molinagranda@gmail.com>
2025-08-23 17:21:08 -0300

    Creacion de IDs en HTML, getElementById y .inneHTML

---
+++ JavaScript/17-DOM/main.js
@@ -0,0 +1,17 @@
+// window.addEventListener("DOMContentLoaded", () => {
+
+// Colocar tu Script haciendo que no sea necesirio colocar el DEFER en el HTML
+
+// });
+
+
+// DOM (Document Object Model) 
+
+// getElementById - Seleccionar elementos por su ID
+
+let caja = document.getElementById("caja");
+console.log(caja);
+
+// innerHTML - Modificar el contenido HTML de un elemento
+
+caja.innerHTML = "Texto modificado desde JS";
