8a087996b1f0d5861e8daf45ef4961ef752eec37
Ignacio Molina <molinagranda@gmail.com>
2025-08-23 17:33:33 -0300

    .style, .className, .classList, .classList.add

--- JavaScript/17-DOM/main.js
+++ JavaScript/17-DOM/main.js
@@ -15,3 +15,29 @@ console.log(caja);
 // innerHTML - Modificar el contenido HTML de un elemento
 
 caja.innerHTML = "Texto modificado desde JS";
+
+// style - Modificar estilos CSS de un elemento
+
+//caja.style.(elemento que quieras modificar de CSS) = "valor";
+
+/*
+caja.style.background = "gray";
+caja.style.maxWidth = "50%";
+caja.style.margin = "0 auto";
+*/
+
+function fondoCaja(color, radius) {
+    caja.style.background = color;
+    caja.style.borderRadius = radius;
+}
+
+// className - Devolver el nombre de la clase CSS de un elemento
+console.log(caja.className);
+
+// classList - Asignar clases CSS a un elemento
+caja.classList = "cajita cajaza"; // Reemplaza todas las clases que tenga el elemento
+
+// classList.add - Añadir una clase CSS a un elemento
+caja.classList.add("cajita");
+
+
