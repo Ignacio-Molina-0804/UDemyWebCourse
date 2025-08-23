c85a6d514d725ee415d42a6527dab7c373565fd4
Ignacio Molina <molinagranda@gmail.com>
2025-08-23 18:43:18 -0300

    getElementsByClassName, .createElement, .createTextNode

--- JavaScript/17-DOM/main.js
+++ JavaScript/17-DOM/main.js
@@ -1,11 +1,10 @@
-// window.addEventListener("DOMContentLoaded", () => {
+// window.addEventListener("DOMContentLoaded", function() => {
 
 // Colocar tu Script haciendo que no sea necesirio colocar el DEFER en el HTML
 
 // });
 
-
-// DOM (Document Object Model) 
+// DOM (Document Object Model)
 
 // getElementById - Seleccionar elementos por su ID
 
@@ -27,8 +26,8 @@ caja.style.margin = "0 auto";
 */
 
 function fondoCaja(color, radius) {
-    caja.style.background = color;
-    caja.style.borderRadius = radius;
+  caja.style.background = color;
+  caja.style.borderRadius = radius;
 }
 
 // className - Devolver el nombre de la clase CSS de un elemento
@@ -40,4 +39,34 @@ caja.classList = "cajita cajaza"; // Reemplaza todas las clases que tenga el ele
 // classList.add - Añadir una clase CSS a un elemento
 caja.classList.add("cajita");
 
+// getElementsByClassName - Seleccionar elementos por su clase CSS
 
+let articulos = document.getElementsByClassName("article");
+console.log(articulos);
+
+for (let i = 0; i < articulos.length; i++) {
+  articulos[i].classList.add("articleBase");
+
+  // Añadir un nodo del DOM
+  let enlace = document.createElement("a");
+  enlace.setAttribute("href", "https://google.com");
+  enlace.setAttribute("target", "_blank");
+  enlace.style.color = "green";
+
+  let textoEnlace = document.createTextNode("Sigue leyendo");
+  enlace.append(textoEnlace);
+
+  articulos[i].append(enlace);
+
+  if ((i + 1) % 2 == 0) {
+    articulos[i].classList.add("articlePar");
+    articulos[i].innerHTML += "<hr> Artículo par modificado desde JS";
+  }
+
+  if (i === articulos.length - 1) {
+    articulos[i].classList.add("articleUltimo");
+    articulos[i].innerHTML += "<hr> Último artículo modificado desde JS";
+  }
+}
+
+// getElementsByTagName - Seleccionar elementos por su etiqueta HTML
