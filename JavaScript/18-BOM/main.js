ca845d7f63bfc25c49355c6c74480fc9e2e45461
Ignacio Molina <molinagranda@gmail.com>
2025-08-24 20:33:59 -0300

    Window Propiedades Basicas

--- JavaScript/18-BOM/main.js
+++ JavaScript/18-BOM/main.js
@@ -11,3 +11,27 @@ console.log(nombre);
 
 // Confirm
 confirm("¿Estás seguro de querer continuar?");
+
+/// Window - Ventana del Navegador
+
+console.log(window)
+
+// Propiedades
+
+console.log(window.innerHeight); // Alto de la ventana
+console.log(window.innerWidth); // Ancho de la ventana
+
+console.log(window.outerHeight, window.outerWidth); // Alto y ancho de la ventana incluyendo barras de navegador
+
+console.log(window.closed); // Devuelve true o false si la ventana está cerrada o no
+console.log(window.document); // Devuelve el objeto document
+console.log(window.navigator); // Devuelve el objeto navigator
+console.log(window.navigator.userAgent); // Devuelve el userAgent del navegador
+
+window.setInterval(() => {
+  console.log("Hola");
+}, 1000); // Ejecuta una función cada cierto tiempo (milisegundos)
+
+console.log(window.scrollX, window.scrollY); // Devuelve la posición de los ejes X y Y
+
+
