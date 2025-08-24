cdf63ffaaf4459bc2068264bcd4a5fcb9fc356b3
Ignacio Molina <molinagranda@gmail.com>
2025-08-24 20:26:41 -0300

    Usos Basicos Vistos Anteriormente

--- JavaScript/18-BOM/main.js
+++ JavaScript/18-BOM/main.js
@@ -1,3 +1,13 @@
 // BOM - Browser Object Model
 
 /// Alertas y Dialogos
+
+// Alertas
+alert("Hola, soy una alerta");
+
+// Prompt
+let nombre = prompt("¿Cuál es tu nombre?");
+console.log(nombre);
+
+// Confirm
+confirm("¿Estás seguro de querer continuar?");
