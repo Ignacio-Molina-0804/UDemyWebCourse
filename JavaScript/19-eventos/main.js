ca0e1422872b583da11f3f973ed10b947d653422
Ignacio Molina <molinagranda@gmail.com>
2025-08-25 20:55:37 -0300

    Datos sobre los Eventos

--- JavaScript/19-eventos/main.js
+++ JavaScript/19-eventos/main.js
@@ -2,9 +2,19 @@
 
 let botoncito = document.querySelector("#botoncito")
 
+// Evento Click 
+
 botoncito.addEventListener("click", () => {
 
     alert("Haz dado click al botoncito!!")
+
+})
+
+// Sacar datos del evento que ejecutamos
+
+botoncito.addEventListener("click", (event) => {
+
+    console.log(event)
 
 })
 
