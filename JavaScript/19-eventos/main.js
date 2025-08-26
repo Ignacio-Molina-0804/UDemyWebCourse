6a54ab915aa3ad749b1ab3a10d0cebf86f1130fe
Ignacio Molina <molinagranda@gmail.com>
2025-08-26 20:53:35 -0300

    Evento click, dblclick, mouseover, mouseout, mousemove

--- JavaScript/19-eventos/main.js
+++ JavaScript/19-eventos/main.js
@@ -1,20 +1,47 @@
 /// Eventos
 
-let botoncito = document.querySelector("#botoncito")
-
-// Evento Click 
-
-botoncito.addEventListener("click", () => {
-
-    alert("Haz dado click al botoncito!!")
-
-})
+let botoncito = document.querySelector("#botoncito");
 
 // Sacar datos del evento que ejecutamos
 
 botoncito.addEventListener("click", (event) => {
+  console.log(event);
+});
 
-    console.log(event)
+// Evento Click
+
+botoncito.addEventListener("click", () => {
+  alert("Haz dado click al botoncito!!");
+});
+
+// Evento Doble Click
+
+let botoncitoDoble = document.querySelector("#botoncitoDoble");
+botoncitoDoble.addEventListener("dblclick", () => {
+  alert("Haz dado dos clicks al botoncito!!");
+});
+
+// Evento Mouse Over
+
+let cajaTexto = document.querySelector("#cajaTexto");
+cajaTexto.addEventListener("mouseover", () => {
+
+    console.log("Haz pasado por encima de la textArea");
 
 })
 
+// Evento Mouse Out
+
+cajaTexto.addEventListener("mouseout", () => {
+
+    console.log("Haz salido del textArea");
+
+})
+
+// Evento Mouse Move
+
+cajaTexto.addEventListener("mousemove", () => {
+
+    console.log("Te estas moviendo dentro del textArea");
+
+})
