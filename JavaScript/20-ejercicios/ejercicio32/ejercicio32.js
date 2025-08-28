5f4cbc4b4083b5762b2c8499f6130fd27a482cc5
Ignacio Molina <molinagranda@gmail.com>
2025-08-27 21:58:55 -0300

    Estructura Ejercicio 32

---
+++ JavaScript/20-ejercicios/ejercicio32/ejercicio32.js
@@ -0,0 +1,13 @@
+/*
+Ejercicio 32:
+
+Crea una lista de la compra en la que el usuario pueda añadir 
+nuevos productos para comprar con un campo de texto y un botón.
+
+Cada vez que se añade un producto, este aparece 
+en la lista con un botón para eliminarlo.
+
+El usuario podrá añadir tantos productos como quiera.
+Cada producto añadido tiene un botón para borrarlo de la lista.
+
+*/
