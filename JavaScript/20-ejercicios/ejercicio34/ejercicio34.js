e8e66c53d342969f9a66156484c17555879f26de
Ignacio Molina <molinagranda@gmail.com>
2025-08-28 21:17:34 -0300

    Estructura Basica Ejercicio 34

---
+++ JavaScript/20-ejercicios/ejercicio34/ejercicio34.js
@@ -0,0 +1,10 @@
+/*
+Ejercicio 34:
+
+Crea un programa que abra una nueva ventana con una web,
+pero en diferentes tamaños.
+
+Haz que el usuario pueda elegir entre tres tamaños (pequeño, mediano y grande).
+*/
+
+
