c3df122046473e7eb6cbd8614d42522ae766a396
Ignacio Molina <molinagranda@gmail.com>
2025-08-28 21:25:02 -0300

    Ejercicio 35 Estructura Basica

---
+++ JavaScript/20-ejercicios/ejercicio35/ejercicio35.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 35:
+
+Crea un programa que permita mover la ventana a diferentes posiciones
+en la pantalla.
+
+Usa botones para mover la ventana a la esquina superior izquierda,
+superior derecha, inferior izquierda e inferior derecha.
+*/
