5cf7beb32b082a047639a03f65bde2e0e723270b
Ignacio Molina <molinagranda@gmail.com>
2025-08-29 14:56:02 -0300

    Ejercicio 37 Estructura Basica

---
+++ JavaScript/20-ejercicios/ejercicio37/ejercicio37.js
@@ -0,0 +1,10 @@
+/* 
+
+Ejercicio 37:
+
+    1. Crea un formulario con estos campos: Nombre, Apellido y Edad.
+    2. Añade un boton de enviar y usa el evento submit.
+    3. Muestra los datos por pantalla cuando se termine de enviar el form.
+
+*/
+
