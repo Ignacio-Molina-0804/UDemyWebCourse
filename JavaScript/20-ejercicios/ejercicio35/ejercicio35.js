5029d300b778874805d70127c2ab13830c2375f9
Ignacio Molina <molinagranda@gmail.com>
2025-08-29 14:41:22 -0300

    Ejercicio 35

--- JavaScript/20-ejercicios/ejercicio35/ejercicio35.js
+++ JavaScript/20-ejercicios/ejercicio35/ejercicio35.js
@@ -7,3 +7,43 @@ en la pantalla.
 Usa botones para mover la ventana a la esquina superior izquierda,
 superior derecha, inferior izquierda e inferior derecha.
 */
+
+const anchura = window.screen.width;
+const altura = window.screen.height;
+
+const btnVentana = document.querySelector("#ventana");
+
+const arribaIZQ = document.querySelector("#arribaIZQ");
+const arribaDER = document.querySelector("#arribaDER");
+const abajoIZQ = document.querySelector("#abajoIZQ");
+const abajoDER = document.querySelector("#abajoDER");
+
+let miVentana = null;
+
+btnVentana.addEventListener("click", () => {
+  miVentana = window.open("", "Nueva Ventana", "width=400,height=400");
+});
+
+arribaIZQ.addEventListener("click", () => {
+  if (miVentana) {
+    miVentana.moveTo(0, 0);
+  }
+});
+
+arribaDER.addEventListener("click", () => {
+  if (miVentana) {
+    miVentana.moveTo(anchura - miVentana.outerWidth , 0);
+  }
+});
+
+abajoIZQ.addEventListener("click", () => {
+  if (miVentana) {
+    miVentana.moveTo(0, altura - miVentana.outerHeight);
+  }
+});
+
+abajoDER.addEventListener("click", () => {
+  if (miVentana) {
+    miVentana.moveTo(anchura - miVentana.outerWidth, altura - miVentana.outerHeight);
+  }
+});
