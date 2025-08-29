fed00103a17fef7caa578daac5dec503ed3da624
Ignacio Molina <molinagranda@gmail.com>
2025-08-29 14:49:24 -0300

    Ejercicio 36

--- JavaScript/20-ejercicios/ejercicio36/ejercicio36.js
+++ JavaScript/20-ejercicios/ejercicio36/ejercicio36.js
@@ -8,3 +8,13 @@ el usuario dentro de la caja.
 Muestra el numero de teclas pulsadas en tiempo real en la web.
 
 */
+
+let contador = document.querySelector("#contador");
+let cajaPulsaciones = document.querySelector("#pulsaciones");
+
+let pulsaciones = 0
+
+contador.addEventListener("input", () => {
+    pulsaciones++
+    cajaPulsaciones.textContent = `Pulsaciones: ${pulsaciones}`;
+});
