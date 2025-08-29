9d5c29efce70dee3c36e380df402f698bbf89154
Ignacio Molina <molinagranda@gmail.com>
2025-08-28 21:22:45 -0300

    Ejercicio 34 Completo

--- JavaScript/20-ejercicios/ejercicio34/ejercicio34.js
+++ JavaScript/20-ejercicios/ejercicio34/ejercicio34.js
@@ -7,4 +7,28 @@ pero en diferentes tamaños.
 Haz que el usuario pueda elegir entre tres tamaños (pequeño, mediano y grande).
 */
 
+let pequena = document.querySelector("#pequena");
+let mediana = document.querySelector("#mediana");
+let grande = document.querySelector("#grande");
 
+let ventana = (anchura, altura) => {
+  window.open(
+    "https://google.com",
+    "_blank",
+    `width=${anchura},height=${altura}`
+  );
+
+  return true;
+};
+
+pequena.addEventListener("click", () => {
+  ventana(250, 250);
+});
+
+mediana.addEventListener("click", () => {
+  ventana(500, 500);
+});
+
+grande.addEventListener("click", () => {
+  ventana(850, 750);
+});
