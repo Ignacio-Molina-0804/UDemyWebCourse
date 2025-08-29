dd78b06c3f4f4195ece345148575a40d823cfe65
Ignacio Molina <molinagranda@gmail.com>
2025-08-29 14:44:06 -0300

    Estructura Basica Ejercicio 36

---
+++ JavaScript/20-ejercicios/ejercicio36/ejercicio36.js
@@ -0,0 +1,10 @@
+/*
+
+Ejercicio 36:
+
+Crea una caja de texto que cuente cuantas teclas ha pulsado
+el usuario dentro de la caja.
+
+Muestra el numero de teclas pulsadas en tiempo real en la web.
+
+*/
