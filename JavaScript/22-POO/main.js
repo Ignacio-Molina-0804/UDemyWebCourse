71da459b65c72e42c8690fdbf568d16cb60408a1
Ignacio Molina <molinagranda@gmail.com>
2025-08-31 18:54:28 -0300

    Get y Set con metodos y palabras reservadas de JavaScript

--- JavaScript/22-POO/main.js
+++ JavaScript/22-POO/main.js
@@ -2,28 +2,52 @@
 
 // Molde
 class Gato {
+  // Le podemos dar como un valor por defecto en nombre de esta manera
+  constructor(nombre = "MishiMishi", raza, color) {
+    // Propiedades o atributos (Caracteristicas)
+    this.nombre = nombre;
+    this.raza = raza;
+    this.color = color;
+  }
 
-    constructor(){
+  // Metodos (Acciones)
 
-        // Propiedades o atributos (Caracteristicas)
-        this.nombre = "Pepe"
-        this.raza = "Abisinio"
-        this.color = "Marron"
+  maullar() {
+    console.log(`El Gato ${this.nombre} dice: ¡Miau Miau!`);
+  }
 
-    }
+  saltar() {
+    console.log(`¡El Gato ${this.nombre} ha saltado!`);
+  }
 
-    // Metodos (Acciones)
+  // Get y Set con metodos normales
+  getRaza() {
+    return this.raza;
+  }
+  setRaza(raza) {
+    this.raza = raza;
+  }
 
-    maullar(){
-
-        console.log(`El Gato ${this.nombre} dice: ¡Miau Miau!`)
-
-    }
-
-    saltar(){
-
-        console.log(`¡El Gato ${this.nombre} ha saltado!`)
-
-    }
- 
+  // Get y Set con las palabras reservadas de JavaScript
+  get sacarColor() {
+    return this.color;
+  }
+  set guardarColor(color) {
+    this.color = color;
+  }
 }
+
+// Creacion de objeto con los atributos necesarios
+let miGato = new Gato("Pepe", "Pardo", "Marron");
+
+// Setter y Getter de la raza con metodos habituales
+miGato.setRaza("Huerfano")
+console.log(miGato.getRaza());
+
+// Setter y Getter con las palabras reservadas
+miGato.guardarColor = "Negro"
+console.log(miGato.sacarColor)
+
+// Uso de Metodos
+miGato.maullar();
+miGato.saltar();
