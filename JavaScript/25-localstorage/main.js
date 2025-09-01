11f148bfb9071878d1c126c9883d94ec5acca99d
Ignacio Molina <molinagranda@gmail.com>
2025-09-01 19:04:31 -0300

    Guardar Objetos, Recuperar Objeto, Eliminar Elementos y Vaciar LocalStorage

--- JavaScript/25-localstorage/main.js
+++ JavaScript/25-localstorage/main.js
@@ -8,9 +8,35 @@ if (typeof Storage !== "undefined") {
 }
 
 // Guardar Datos
-localStorage.setItem("curso", "Curso WEB")
-localStorage.setItem("link", "https://github.com/Ignacio-Molina-0804/UDemyWebCourse")
+localStorage.setItem("curso", "Curso WEB");
+localStorage.setItem(
+  "link",
+  "https://github.com/Ignacio-Molina-0804/UDemyWebCourse"
+);
 
 // Sacar Datos
-document.querySelector("#curso").textContent = localStorage.getItem("curso")
-document.querySelector("#enlace").textContent = console.log(localStorage.getItem("enlace"))
+document.querySelector("#curso").textContent = localStorage.getItem("curso");
+document.querySelector("#enlace").textContent = console.log(
+  localStorage.getItem("enlace")
+);
+
+// Guardar Objetos
+let animal = {
+  tipo: "Elefante",
+  nombre: "Trompita",
+  color: "Gris",
+};
+
+localStorage.setItem("animal", JSON.stringify(animal));
+
+// Recuperar un Objeto
+let animalJS = JSON.parse(localStorage.getItem("animal"));
+console.log(animalJS);
+
+// Eliminar Elementos
+localStorage.removeItem("animal");
+
+// Vaciar Storage
+document.querySelector("#vaciar").addEventListener("click", () => {
+  localStorage.clear();
+});
