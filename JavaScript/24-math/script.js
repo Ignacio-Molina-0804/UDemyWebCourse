781f3da9d5d2d710261ad5295b4473a0f8daad95
Ignacio Molina <molinagranda@gmail.com>
2025-09-01 17:33:59 -0300

    Math.PI, Math.E, Math.round, Math.floor, Math.ceil

---
+++ JavaScript/24-math/script.js
@@ -0,0 +1,17 @@
+/// Objeto Math
+
+// Numero PI
+console.log(Math.PI);
+
+// Numero Euler
+console.log(Math.E);
+
+// Redondear al entero mas cercano
+console.log(Math.round(7.2)) // 7
+console.log(Math.round(7.6)) // 8
+
+// Redondear al numero mas bajo
+console.log(Math.floor(7.9))
+
+// Redondear al numero mas alto
+console.log(Math.ceil(7.01))
