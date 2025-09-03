c2d291145d861e6055d113afc27107bc90d1214b
Ignacio Molina <molinagranda@gmail.com>
2025-09-03 17:58:14 -0300

    Promesas Encadenadas

--- JavaScript/28-promesas/main.js
+++ JavaScript/28-promesas/main.js
@@ -25,6 +25,51 @@ miPromesa
     console.error("Error: " + error);
   });
 
-console.log("Hola")
-console.log("Soy")
-console.log("Pepe")
+console.log("Hola");
+console.log("Soy");
+console.log("Pepe");
+
+/// Promesas Encadenadas
+
+function servirPizza() {
+  // 15 minutos para entregar la pizza
+
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("Pizza Servida"), 15000);
+  });
+}
+
+function servirHamburguesa() {
+  // 8 minutos para entregar la pizza
+
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("Hamburguesa Servida"), 8000);
+  });
+}
+
+function servirSolomilloSalsaAceituna() {
+  // 12 minutos para entregar la pizza
+
+  return new Promise((resolve) => {
+    setTimeout(
+      () => resolve("Solomillo con Salsa de Aceitunas Servida"),
+      12000
+    );
+  });
+}
+
+servirPizza()
+  .then((resultado1) => {
+    console.log(resultado1);
+    return servirHamburguesa();
+  })
+  .then((resultado2) => {
+    console.log(resultado2);
+    return servirSolomilloSalsaAceituna;
+  })
+  .then((resultado3) => {
+    console.log(resultado3);
+  })
+  .catch((error) => {
+    console.log("Error en las comandas: " + error);
+  });
