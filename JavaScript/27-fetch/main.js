4e31007396aa64cb4b35d135b01cbb0a238003ed
Ignacio Molina <molinagranda@gmail.com>
2025-09-03 16:58:54 -0300

    Fetch GET

--- JavaScript/27-fetch/main.js
+++ JavaScript/27-fetch/main.js
@@ -1,1 +1,28 @@
-// FETCH (ajax)
+/// FETCH (ajax)
+
+// Sacar informacion de manera asincrona (GET)
+
+let idUser = 4
+
+fetch("https://reqres.in/api/users/"+idUser)
+    .then(response => response.json())
+    .then(data => {
+
+        console.log(data.data)
+
+    })
+    .catch(error =>{
+
+        console.error("Error al pedir los datos del servidor ", error)
+
+    })
+
+function mostrarUsuario(user){
+
+    let caja = document.querySelector("#user")
+    caja.innerHTML = `
+        <h2>${user.first_name} ${user.last_name}</h2>
+        <img src="${user.avatar}"/>
+    `
+
+}
