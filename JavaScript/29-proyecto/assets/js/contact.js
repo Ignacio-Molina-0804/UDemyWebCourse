26b785b4d39ff0dd9371bdc88835fd3a3510ce52
Ignacio Molina <molinagranda@gmail.com>
2025-09-07 22:43:16 -0300

    Arreglos en el Form

--- JavaScript/29-proyecto/assets/js/contact.js
+++ JavaScript/29-proyecto/assets/js/contact.js
@@ -3,41 +3,41 @@ window.addEventListener("load", () => {
   let isValidate = false;
 
   validation
-    .addField("#name", [
+    .addField("#contactName", [
       {
         rule: "required",
         errorMessage: "El nombre es obligatorio",
       },
       {
-        rule: "minLenght",
+        rule: "minLength",
         value: 3,
         errorMessage: "El nombre es muy corto!!",
       },
       {
-        rule: "maxLenght",
+        rule: "maxLength",
         value: 15,
         errorMessage: "El nombre es muy largo!!",
       },
     ])
 
-    .addField("#surname", [
+    .addField("#contactSurname", [
       {
         rule: "required",
         errorMessage: "Los apellidos son obligatorios",
       },
       {
-        rule: "minLenght",
+        rule: "minLength",
         value: 3,
         errorMessage: "Los apellidos son muy cortos!!",
       },
       {
-        rule: "maxLenght",
+        rule: "maxLength",
         value: 25,
         errorMessage: "Los apellidos son muy largos!!",
       },
     ])
 
-    .addField("#email", [
+    .addField("#contactEmail", [
       {
         rule: "required",
         errorMessage: "El email es obligatorio!",
@@ -47,19 +47,19 @@ window.addEventListener("load", () => {
         errorMessage: "El email no es valido!",
       },
     ])
-    .addField("#gender", [
+    .addField("#contactGender", [
       {
         rule: "required",
         errorMessage: "Selecciona un genero!",
       },
     ])
-    .addField("#date", [
+    .addField("#contactDate", [
       {
         rule: "required",
-        errorMessage: "Selecciona te fecha de nacimiento!",
+        errorMessage: "Selecciona tu fecha de nacimiento!",
       },
     ])
-    .addField("#years", [
+    .addField("#contactYears", [
       {
         rule: "required",
         errorMessage: "La edad es obligatoria!",
