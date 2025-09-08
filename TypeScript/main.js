9cd6f6e040d2b01a823a5b6b444da262bc41a15d
Ignacio Molina <molinagranda@gmail.com>
2025-09-08 16:48:14 -0300

    Creacion en TypeScript

---
+++ TypeScript/main.js
@@ -0,0 +1,2 @@
+// Para que esto funcione en el navegador lo hay que transformar en JavaScript poniendo "npx tsc (nombreDeTuArchivo).ts"
+var nombre = "Ignacio Molina";
---
+++ TypeScript/main.ts
@@ -0,0 +1,3 @@
+// Para que esto funcione en el navegador lo hay que compilar en JavaScript poniendo "npx tsc (nombreDeTuArchivo).ts"
+
+let nombre:string = "Ignacio Molina"
