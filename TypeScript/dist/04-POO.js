a2f51b1014e1d56333aa5be5e5c4c31e4af14f92
Ignacio Molina <molinagranda@gmail.com>
2025-09-08 17:51:02 -0300

    POO en TS

---
+++ TypeScript/dist/04-POO.js
@@ -0,0 +1,55 @@
+"use strict";
+// Programacion Orientada a Objetos (POO)
+Object.defineProperty(exports, "__esModule", { value: true });
+// Molde
+class Gato {
+    nombre; // Puclic deja llamar a la propiedad fuera de la clase
+    raza; // Private deja llamar a la propiedad solo dentro de la clase
+    color; // Protected deja llamar a la propiedad dentro y en las clases que hereden la padre
+    // Le podemos dar como un valor por defecto en nombre de esta manera
+    constructor(nombre = "MishiMishi", raza, color) {
+        // Propiedades o atributos (Caracteristicas)
+        this.nombre = nombre;
+        this.raza = raza;
+        this.color = color;
+    }
+    // Metodos (Acciones)
+    maullar() {
+        console.log(`El Gato ${this.nombre} dice: ¡Miau Miau!`);
+    }
+    saltar() {
+        console.log(`¡El Gato ${this.nombre} ha saltado!`);
+    }
+    // Get y Set con metodos normales
+    getRaza() {
+        return this.raza;
+    }
+    setRaza(raza) {
+        this.raza = raza;
+    }
+    // Get y Set con las palabras reservadas de JavaScript
+    get sacarColor() {
+        return this.color;
+    }
+    set guardarColor(color) {
+        this.color = color;
+    }
+    // Metodo Estatico
+    // Se utiliza para llamarlo sin necesidad de crear un objeto
+    static saludar() {
+        alert("Hola Amigo, que tal estas?");
+    }
+}
+// Creacion de objeto con los atributos necesarios
+let miGato = new Gato("Pepe", "Pardo", "Marron");
+// Setter y Getter de la raza con metodos habituales
+miGato.setRaza("Huerfano");
+console.log(miGato.getRaza());
+// Setter y Getter con las palabras reservadas
+miGato.guardarColor = "Negro";
+console.log(miGato.sacarColor);
+// Uso de Metodos
+miGato.maullar();
+miGato.saltar();
+// Uso del Metodo Estatico
+Gato.saludar();
