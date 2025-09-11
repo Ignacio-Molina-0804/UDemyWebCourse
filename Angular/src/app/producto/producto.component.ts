73f5a83f656b75f90741874ab0682a635abb7dd5
Ignacio Molina <molinagranda@gmail.com>
2025-09-11 17:02:58 -0300

    Uso de Componentes en un componente

--- Angular/src/app/producto/producto.component.ts
+++ Angular/src/app/producto/producto.component.ts
@@ -1,28 +1,27 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { Usuario } from '../usuario/usuario';
+import { Pelicula } from '../pelicula/pelicula';
+import { Ventas } from '../ventas/ventas';
 
 @Component({
-    selector: "miProducto",
-    templateUrl: "producto.component.html",
-    styleUrl: "producto.component.css"
+  selector: 'miProducto',
+  imports: [Usuario, Pelicula, Ventas],
+  templateUrl: 'producto.component.html',
+  styleUrl: 'producto.component.css',
 })
+export class ProductoComponent {
+  public nombreDelProducto: string;
+  public marca: string;
+  public descripcion: string;
+  public precio: number;
 
-export class ProductoComponent{
+  constructor() {
+    this.nombreDelProducto = 'Pc Portatil';
+    this.descripcion = 'Mi producto es una computadora muy potente.';
+    this.marca = 'Asus';
+    this.precio = 190000;
 
-    public nombreDelProducto:string;
-    public marca:string;
-    public descripcion:string;
-    public precio:number;
-
-    constructor(){
-
-        this.nombreDelProducto = "Pc Portatil"
-        this.descripcion = "Mi producto es una computadora muy potente."
-        this.marca = "Asus"
-        this.precio = 190000
-
-        console.log("Se ha cargado el componente de productos!")
-        alert("Holaa")
-
-    }
-
+    console.log('Se ha cargado el componente de productos!');
+    alert('Holaa');
+  }
 }
