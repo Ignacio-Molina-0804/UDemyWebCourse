d38cb9a1c0ecb24a34f9c7a6b41635721d1abd35
Ignacio Molina <molinagranda@gmail.com>
2025-09-11 00:25:48 -0300

    Plantillas y propiedas en Componentes

--- Angular/src/app/producto/producto.component.ts
+++ Angular/src/app/producto/producto.component.ts
@@ -8,7 +8,17 @@ import { Component } from "@angular/core";
 
 export class ProductoComponent{
 
+    public nombreDelProducto:string;
+    public marca:string;
+    public descripcion:string;
+    public precio:number;
+
     constructor(){
+
+        this.nombreDelProducto = "Pc Portatil"
+        this.descripcion = "Mi producto es una computadora muy potente."
+        this.marca = "Asus"
+        this.precio = 190000
 
         console.log("Se ha cargado el componente de productos!")
         alert("Holaa")
