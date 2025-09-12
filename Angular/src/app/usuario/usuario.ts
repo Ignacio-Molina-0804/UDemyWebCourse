d7520dba6af02b7feadf82fc51fa64e2e5d25821
Ignacio Molina <molinagranda@gmail.com>
2025-09-12 16:17:40 -0300

    Condicional Switch

--- Angular/src/app/usuario/usuario.ts
+++ Angular/src/app/usuario/usuario.ts
@@ -1,10 +1,11 @@
 // Componentes Creados con el comango "ng generate component (Nombre del componente) / ng g c (Nombre del componente)"
 
 import { Component } from '@angular/core';
+import { Ventas } from '../ventas/ventas';
 
 @Component({
   selector: 'app-usuario',
-  imports: [],
+  imports: [Ventas],
   templateUrl: './usuario.html',
   styleUrl: './usuario.css',
 })
@@ -24,6 +25,15 @@ export class Usuario {
     {id:5, nombre:"Adidas"},
     {id:6, nombre:"Starvite"}
   ]
+
+  usuario = {
+
+    id: 1,
+    nombre: "Ignacio Molina",
+    nick: "NachitoMolina",
+    rol: "admin"
+
+  };
 
   cambioRedireccion() {
     if (this.redirigir == false) {
