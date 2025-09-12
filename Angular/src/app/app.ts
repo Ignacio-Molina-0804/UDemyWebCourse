d0f6f4ad8b0c40459a2b57be45cc86a2347d70cf
Ignacio Molina <molinagranda@gmail.com>
2025-09-12 15:31:26 -0300

    Aclaraciones de Uso

--- Angular/src/app/app.ts
+++ Angular/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { ProductoComponent } from './producto/producto.component';
+import { Usuario } from './usuario/usuario';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductoComponent],
+  imports: [Usuario],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Este es mi primer proyecto de Angular!');
 }
+ 
