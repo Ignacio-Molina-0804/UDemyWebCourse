59be48f0ffd5cd58605ba6b48a4e26211c2873d6
Ignacio Molina <molinagranda@gmail.com>
2025-09-13 20:32:56 -0300

    NgStyle

--- Angular/src/app/ventas/ventas.ts
+++ Angular/src/app/ventas/ventas.ts
@@ -2,16 +2,22 @@
 
 import { Component } from '@angular/core';
 import { Pelicula } from '../pelicula/pelicula';
-import { NgClass } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-ventas',
-  imports: [Pelicula, NgClass],
+  imports: [NgClass, NgStyle],
   templateUrl: './ventas.html',
-  styleUrl: './ventas.css'
+  styleUrl: './ventas.css',
 })
 export class Ventas {
-
   public colorear: boolean = true;
 
+  public misEstilos = {
+    'background-color': 'blue',
+    'font.size': '20px',
+    'border-radius': '40px',
+    padding: '10px',
+    color: 'white',
+  };
 }
