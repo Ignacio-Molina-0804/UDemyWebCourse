8f35d400e50fa2f3da2276880bf42c4717d8527c
Ignacio Molina <molinagranda@gmail.com>
2025-09-15 18:38:09 -0300

    Agregado a Peliculas Principal con ID automatico

--- Angular/src/app/components/cine/cine.ts
+++ Angular/src/app/components/cine/cine.ts
@@ -12,7 +12,7 @@ export class Cine {
   public titulo: string;
   public peliculas: Array<Pelicula>;
 
-  public mi_pelicula: string = "";
+  public mi_pelicula: string = '';
   public pelisSinDatos: string[] = [];
 
   constructor() {
@@ -23,7 +23,15 @@ export class Cine {
       new Pelicula(3, 'Inception', 'Ciencia Ficción', 'Christopher Nolan', 2010, 'hbo', false),
       new Pelicula(4, 'Forrest Gump', 'Drama', 'Robert Zemeckis', 1994, 'disney+', true),
       new Pelicula(5, 'Gladiador', 'Acción', 'Ridley Scott', 2000, 'prime', false),
-      new Pelicula(6, 'El Señor de los Anillos', 'Fantasía', 'Peter Jackson', 2001, 'netflix', true),
+      new Pelicula(
+        6,
+        'El Señor de los Anillos',
+        'Fantasía',
+        'Peter Jackson',
+        2001,
+        'netflix',
+        true
+      ),
       new Pelicula(7, 'Matrix', 'Ciencia Ficción', 'Lana y Lilly Wachowski', 1999, 'hbo', false),
       new Pelicula(8, 'Avatar', 'Ciencia Ficción', 'James Cameron', 2009, 'disney+', true),
       new Pelicula(9, 'Joker', 'Drama', 'Todd Phillips', 2019, 'hbo', true),
@@ -32,29 +40,30 @@ export class Cine {
     ];
   }
 
-  ngOnInit(){
+  ngOnInit() {
+    console.log(this.peliculas);
 
-    console.log(this.peliculas)
-
-    this.peliculas[1].titulo = "ET, El marciano"
-
+    this.peliculas[1].titulo = 'ET, El marciano';
   }
 
-  ngDoCheck(){
-
-    console.log(this.mi_pelicula)
-
+  ngDoCheck() {
+    console.log(this.mi_pelicula);
   }
 
-  showPelicula(){
-
-    alert(this.mi_pelicula)
-
+  showPelicula() {
+    alert(this.mi_pelicula);
   }
 
-  addPelicula(){
-
+  addPelicula() {
     this.pelisSinDatos.push(this.mi_pelicula)
+  }
 
+  addPeliculaPrincipal() {
+    let identificador = this.peliculas[this.peliculas.length-1].id + 1;
+    let nuevaPelicula = new Pelicula(identificador, this.mi_pelicula);
+
+    console.log(nuevaPelicula)
+
+    this.peliculas.push(nuevaPelicula);
   }
 }
