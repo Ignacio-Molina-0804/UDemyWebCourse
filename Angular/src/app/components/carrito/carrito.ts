a4ae62f6c7681b41f470a0017ec5ade22477dd33
Ignacio Molina <molinagranda@gmail.com>
2025-09-17 17:23:24 -0300

    Condicionale y Propiedades

--- Angular/src/app/components/carrito/carrito.ts
+++ Angular/src/app/components/carrito/carrito.ts
@@ -8,19 +8,18 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './carrito.css',
 })
 export class Carrito {
-  nombreTienda: string = "Mi Tienda"
+  nombreTienda: string = 'Mi Tienda';
   cantidadProductos: number = 0;
   totalCarrito: number = 0;
   notificacion: string = '';
+  precioBase: number = 37;
 
-  ngOnInit(){
+  ngOnInit() {
     this.comprobarNotificacion();
   }
 
-  ngDoCheck(){
-
-    console.log("Comprobando dom del componente")
-
+  ngDoCheck() {
+    console.log('Comprobando dom del componente');
   }
 
   comprobarNotificacion() {
@@ -33,14 +32,39 @@ export class Carrito {
     }
   }
 
-  addProducto(){
-
+  addProducto() {
     this.cantidadProductos += 1;
-    this.totalCarrito += 37;
+    this.totalCarrito += this.precioBase;
 
-    console.log(`Nuevo productor añadido, Precio total actual ${this.totalCarrito}`)
+    console.log(`Nuevo producto añadido, Precio total actual ${this.totalCarrito}`);
 
-    this.comprobarNotificacion()
+    this.comprobarNotificacion();
+  }
 
+  addCincoProducto() {
+    this.cantidadProductos += 5;
+    this.totalCarrito += this.precioBase * 5;
+
+    console.log(`Cinco nuevos producto añadidos, Precio total actual ${this.totalCarrito}`);
+
+    this.comprobarNotificacion();
+  }
+
+  deleteProducto() {
+    if (this.cantidadProductos >= 1) {
+      this.cantidadProductos -= 1;
+      this.totalCarrito -= this.precioBase;
+
+      console.log(`1 Producto Eliminado, Precio total actual ${this.totalCarrito}`);
+
+      this.comprobarNotificacion();
+    }
+  }
+
+  vaciarCarrito() {
+    this.cantidadProductos = 0;
+    this.totalCarrito = 0;
+    console.log(`Carrito Vaciado, Precio total actual ${this.totalCarrito}`);
+    this.comprobarNotificacion();
   }
 }
