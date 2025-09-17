3782a031b6168985646a938865ddc12e14b298f1
Ignacio Molina <molinagranda@gmail.com>
2025-09-17 18:30:38 -0300

    Creacion de CarritoSignals

---
+++ Angular/src/app/components/carrito-signals/carrito-signals.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrito-signals',
+  imports: [],
+  templateUrl: './carrito-signals.html',
+  styleUrl: './carrito-signals.css'
+})
+export class CarritoSignals {
+
+}
