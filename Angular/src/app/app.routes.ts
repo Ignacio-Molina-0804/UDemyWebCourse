a702046ba14bffdacec5f5e047e1b14a30a012e4
Ignacio Molina <molinagranda@gmail.com>
2025-09-21 15:34:26 -0300

    Routing en Angular y estilizado de navbar general

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { ProductoComponent } from './components/producto/producto.component';
+import { Cine } from './components/cine/cine';
+import { UsuariosHTTP } from './components/usuarios-http/usuarios-http';
+import { Padre } from './components/padre/padre';
+import { CarritoSignals } from './components/carrito-signals/carrito-signals';
+import { Carrito } from './components/carrito/carrito';
+import { Ventas } from './components/ventas/ventas';
+import { Usuario } from './components/usuario/usuario';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'PeticionesHTTP', component: UsuariosHTTP},
+    {path: 'InputOutputDatos', component: Padre},
+    {path: 'Signals', component: CarritoSignals},
+    {path: 'Eventos', component: Carrito},
+    {path: 'TWDB-Services', component: Cine},
+    {path: 'Directivas', component: Ventas},
+    {path: 'ComponenteBasico', component: ProductoComponent},
+    {path: 'Plantillas', component: Usuario},
+
+];
--- Angular/src/app/app.ts
+++ Angular/src/app/app.ts
@@ -8,10 +8,11 @@ import { Carrito } from './components/carrito/carrito';
 import { CarritoSignals } from './components/carrito-signals/carrito-signals';
 import { Padre } from './components/padre/padre';
 import { UsuariosHTTP } from './components/usuarios-http/usuarios-http';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [UsuariosHTTP],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
