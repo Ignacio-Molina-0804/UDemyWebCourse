1d2b32a3bcbcb9c3bd8357f4976e69049b500ae5
Ignacio Molina <molinagranda@gmail.com>
2025-09-22 17:44:45 -0300

    Enrutamiento a Formulario y Formulario Basico

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -8,10 +8,11 @@ import { Carrito } from './components/carrito/carrito';
 import { Ventas } from './components/ventas/ventas';
 import { Usuario } from './components/usuario/usuario';
 import { Pagina404 } from './components/pagina404/pagina404';
+import { Formularios } from './components/formularios/formularios';
 
 export const routes: Routes = [
 
-    {path: 'PeticionesHTTP', component: UsuariosHTTP},
+    {path: 'PeticionesHTTP/:id', component: UsuariosHTTP},
     {path: 'InputOutputDatos', component: Padre},
     {path: 'Signals', component: CarritoSignals},
     {path: 'Eventos', component: Carrito},
@@ -19,6 +20,7 @@ export const routes: Routes = [
     {path: 'Directivas', component: Ventas},
     {path: 'ComponenteBasico', component: ProductoComponent},
     {path: 'Plantillas', component: Usuario},
+    {path: 'Formularios', component: Formularios},
     {path: '**', component: Pagina404},
 
 ];
--- Angular/src/app/components/formularios/formularios.ts
+++ Angular/src/app/components/formularios/formularios.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formularios',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formularios.html',
   styleUrl: './formularios.css'
 })
 export class Formularios {
+
+  nombre = ""
+  email = ""
+  edad = 0
+  password = ""
+
+  onSubmit() {
+  
+    console.log(this.nombre, this.email, this.edad, this.password)
+  
+  }
 
 }
