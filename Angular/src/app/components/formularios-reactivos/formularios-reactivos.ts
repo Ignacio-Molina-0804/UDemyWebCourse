b71dd29b739b2a13545fa5a46b217e03020496f4
Ignacio Molina <molinagranda@gmail.com>
2025-09-22 20:52:00 -0300

    Formulario Reactivo Codeado

--- Angular/src/app/components/formularios-reactivos/formularios-reactivos.ts
+++ Angular/src/app/components/formularios-reactivos/formularios-reactivos.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formularios-reactivos',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './formularios-reactivos.html',
   styleUrl: './formularios-reactivos.css'
 })
 export class FormulariosReactivos {
+
+  formulario = new FormGroup({
+
+    nombre: new FormControl('', [Validators.required, Validators.minLength(3)]), 
+    email: new FormControl('', [Validators.required, Validators.email]),
+    edad: new FormControl('', [Validators.required, Validators.min(18), Validators.max(99)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+
+  })
+
 
 }
