ddbaf8c9671feb24c5b2ab4d17d87a6e53e19634
Ignacio Molina <molinagranda@gmail.com>
2025-09-23 21:26:28 -0300

    Creacion de Rutas y EndPoints en Node

--- Node/index.js
+++ Node/index.js
@@ -1,6 +1,6 @@
 // Importar las dependencias
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // Crear servidor
 const app = express();
@@ -15,12 +15,44 @@ app.use(express.urlencoded({ extended: true }));
 
 // Cargar rutas
 
-
 // Crear endpoints de prueba
+app.get("/", (req, res) => {
+  console.log("La ruta raiz se ha llamado!!");
+
+  return res.status(200).json([
+    {
+      curso: "Master en WEB",
+      url: "https://www.udemy.com/course/master-en-javascript-aprender-js-angular-nodejs-astro-y-mas",
+      profesor: "Victor Robles",
+    },
+    {
+      curso: "Master en BackEnd",
+      url: "https://www.udemy.com/course/master-en-javascript-aprender-js-angular-nodejs-astro-y-mas",
+      profesor: "Victor Robles",
+    },
+    {
+      curso: "Master en JavaScript",
+      url: "https://www.udemy.com/course/master-en-javascript-aprender-js-angular-nodejs-astro-y-mas",
+      profesor: "Victor Robles",
+    },
+  ]);
+});
+
+app.get("/pruebitas", (req, res) => {
+  console.log("Se ha ejecutado el endpoint de prueba");
+
+  return res.status(200).send(
+    `
+        <section>
+            <h1>Estoy aprendiendo Node.js</h1>
+            <h2>Con uDemy</h2>
+            <p>Esto es una prueba de ruta</p>
+        </section>
+        `
+  );
+});
 
 // Poner el servidor a escuchar
 app.listen(port, () => {
-
-    console.log("El servidor está funcionando en http://localhost:" + port);
-
-})
+  console.log("El servidor está funcionando en http://localhost:" + port);
+});
