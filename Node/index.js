3cc3b25cc9c7882c2ee85658329230d39fb8b78a
Ignacio Molina <molinagranda@gmail.com>
2025-09-23 21:18:46 -0300

    Creacion de Servidor en Node

--- Node/index.js
+++ Node/index.js
@@ -1,1 +1,26 @@
-console.log("Estamos Aprendiendo NodeJS con el Nachito Molina");
+// Importar las dependencias
+const express = require('express');
+const cors = require('cors');
+
+// Crear servidor
+const app = express();
+const port = 3977;
+
+// Configurar cors
+app.use(cors());
+
+// Convertir los datos del body a objetos
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cargar rutas
+
+
+// Crear endpoints de prueba
+
+// Poner el servidor a escuchar
+app.listen(port, () => {
+
+    console.log("El servidor está funcionando en http://localhost:" + port);
+
+})
