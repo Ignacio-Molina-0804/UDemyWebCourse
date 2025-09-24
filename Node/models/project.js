25409921cbf750506d9ad9955c7c9e62cbf9ea91
Ignacio Molina <molinagranda@gmail.com>
2025-09-23 21:51:20 -0300

    Creacion Schema

---
+++ Node/models/project.js
@@ -0,0 +1,44 @@
+// Importar la libreria de Mongoose
+const {Schema, model} = require("mongoose")
+
+// Crear esquema (Estructura de cada documento de tipo proyecto)
+const ProjectSchema = new Schema({
+
+    name:{
+
+        type: String,
+        required: true,
+        trim: true
+
+    },
+    description:{
+
+        type: String,
+        required: true
+
+    },
+    state:{
+
+        type: String,
+        required: true
+        
+    },
+    image:{
+
+        type: String,
+        default: "default.png"
+    },
+    created_at:{
+
+        type: Date,
+        default: Date.now
+
+    } 
+
+
+})
+
+// Crear modelo, indicarle la coleccion donde se van a guardar los docs
+
+
+// Exportar el modelo (fichero project.js)
