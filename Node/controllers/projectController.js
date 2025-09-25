28dd48583aa988bdae2f8619b333a754ec9c7cff
Ignacio Molina <molinagranda@gmail.com>
2025-09-24 21:17:53 -0300

    Metodo Save para la API

--- Node/controllers/projectController.js
+++ Node/controllers/projectController.js
@@ -6,12 +6,47 @@ const save = (req, res) => {
     let body = req.body;
 
     // Validar datos
+    if(!body.name || !body.description || !body.state){
+    
+        return res.status(400).send({
+            status: "error",
+            message: "Faltan datos por enviar"
+        });
 
+    }
 
     // Crear objeto
 
+    let projectToSave = new ProjectModel(body);
 
     // Guardo el objeto en la BD
+    projectToSave.save().then(project =>{
+
+        if(!project){
+
+            return res.status(400).send({
+
+                status: "error",
+                message: "El proyecto no se ha guardado correctamente!"
+
+            })
+
+        }
+
+        return res.status(200).send({
+            status: "success",
+            project
+        });
+
+    }).catch(error => {
+
+        return res.status(500).send({
+            status: "error",
+            message: "Faltan datos por enviar",
+            error
+        });
+
+    });
 
     
     // Devolver Respuestas
