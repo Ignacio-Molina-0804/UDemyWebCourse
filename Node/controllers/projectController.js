31d44ad547cbc9c2a469fa0f9e5163b897c6e5f4
Ignacio Molina <molinagranda@gmail.com>
2025-09-24 21:01:26 -0300

    Pasos a seguir en el Metodo Save

--- Node/controllers/projectController.js
+++ Node/controllers/projectController.js
@@ -2,8 +2,24 @@ const ProjectModel = require('../models/projectModel');
 
 const save = (req, res) => {
 
+    // Recibo Datos
+    let body = req.body;
+
+    // Validar datos
+
+
+    // Crear objeto
+
+
+    // Guardo el objeto en la BD
+
+    
+    // Devolver Respuestas
+
     return res.status(200).json({
-        mensaje: "Probando Controlador"
+        mensaje: "Probando Controlador",
+        body
+
     })
 
 }
